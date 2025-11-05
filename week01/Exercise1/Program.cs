c6754ea33eede002342531dd54b7200e41bf5282
tiatuihalangingie <tiafaanunu@gmail.com>
2025-11-04 22:28:53 -0700

    W02\Exercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        // prompt the user for their first name
+        Console.Write("What is your first name? ");
+
+        // read the input string from the console
+        string firstName = Console.ReadLine();
+
+        // prompt the user for their last name
+        Console.Write("What is your last name? ");
+
+        // read the input string from the console
+        string lastName = Console.ReadLine();
+
+        // this will create a blank line
+        Console.WriteLine();
+
+        // display the user entered string
+        Console.WriteLine($"Your name is {lastName}, {firstName}.");
+
     }
+
+
 }
