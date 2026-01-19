5946b73a38cb78064c51af5d2626e890c40670c9
tiatuihalangingie <tiafaanunu@gmail.com>
2026-01-19 13:01:24 -0700

    Week 1 Exersice 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,17 +1,24 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-    
+
+
+        // Prompt User for first name
         Console.Write("What is your first name? ");
+
+        // Variable carry the first name
         string firstName = Console.ReadLine();
-        
+
+        // Prompt the User for the last name.
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
 
+        // Get the first and last name.
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
         
         
