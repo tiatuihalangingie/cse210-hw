da15c63dd854cd9b1031f2f491317379c8885a15
tiatuihalangingie <tiafaanunu@gmail.com>
2026-01-19 13:02:48 -0700

    Week 1 Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Determines the letter grade - Exercise2 Project.");
+
+        // Prompt the User to enter their grade percent
+        Console.Write("Please enter your grade percent? ");
+        string userInput = Console.ReadLine();
+
+        // Convert string to number
+        int grade = int.Parse(userInput);
+
+        // Default value for string letter and sign
+        string letter = "";
+        string sign = "";
+
+        // This math calculation get the last digit of the grade percentage.
+        int lastDigit = Math.Abs(grade% 10);
+
+        // Create a if statement to specify if 7, sign
+        // will be positive or 3 sign will be negative.
+        if (lastDigit == 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit == 3)
+        {
+            sign = "-";
+        }
+
+
+        // Create a if statement to get the right grade letter.
+        if (grade >= 90)
+        {
+            letter = "A";
+            
+        }
+        else if (grade >= 80)
+        {
+            letter ="B";
+        }
+        else if (grade >= 70)
+        {
+            letter ="C";
+        }
+        else if (grade >= 60)
+        {
+            letter ="D";
+        }
+        else
+        {
+            letter = "F";
+            sign = " ";
+        }
+        
+        // Display the letter and sign if it is positive or negative.
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+
+        }
+        else
+        {
+            Console.WriteLine("Please try harder next time!");
+        }
     }
 }
