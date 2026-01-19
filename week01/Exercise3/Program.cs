17d9b80354261ca9786cf82ff4378834e0b5ed47
tiatuihalangingie <tiafaanunu@gmail.com>
2026-01-19 13:03:13 -0700

    Week 1 Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Guessing number game - Exercise3 Project.");
+
+        // This is a random generator that can pick a random number
+        // and let the User guess this random number.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        // This is the default value for guess number.
+        int guess = -1;
+        int manyGuess = 0;
+
+        // Using while loop to keep reiterate if the user 
+        // guess doesn't match the magicNumber.
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+                manyGuess += 1;
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+                manyGuess += 1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You have {manyGuess} guessed");
+                Console.WriteLine("Would you like to play again? Yes/No: ");
+                string answer = Console.ReadLine();
+            
+                if (answer == "Yes")
+                {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine()); 
+
+                if (magicNumber > guess)
+                    {
+                        Console.WriteLine("Higher");
+                        manyGuess += 1;
+                    }
+                else if (magicNumber < guess)
+                    {
+                    Console.WriteLine("Lower");
+                    manyGuess += 1;
+                    }
+                else
+                    {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You have {manyGuess} guessed");
+
+                    }
+            }
+
+            else
+                {
+                Console.WriteLine("Thank you for playing. Have a good day!");
+                }
+                
+            }
+
+        }                    
     }
-}
+}   
+        
+    
